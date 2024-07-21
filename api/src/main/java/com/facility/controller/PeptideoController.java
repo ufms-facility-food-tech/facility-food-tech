f0e6119d1f68316ff8e6122c24930107d46edf4b
caio <hi@caio.sh>
2024-07-21 14:52:33 -0300

    feat(api): streamlined responses

--- api/src/main/java/com/facility/controller/PeptideoController.java
+++ api/src/main/java/com/facility/controller/PeptideoController.java
@@ -2,17 +2,16 @@ package com.facility.controller;
 
 import com.facility.dto.OrganismoDTO;
 import com.facility.dto.PeptideoDTO;
-import com.facility.enums.TipoPeptideo;
 import com.facility.model.Organismo;
 import com.facility.model.Peptideo;
 import com.facility.repository.OrganismoRepository;
 import com.facility.repository.PeptideoRepository;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,37 +33,39 @@ public class PeptideoController {
   private OrganismoRepository organismoRepository;
 
   @PostMapping
-  public ResponseEntity<PeptideoDTO> save(
+  public ResponseEntity<PeptideoDTO> create(
     @RequestBody PeptideoDTO peptideoDTO
   ) {
-    if (peptideoDTO.getOrganismo() == null) {
-      return ResponseEntity.badRequest().build();
-    }
-
-    var organismoEntity = peptideoDTO.getOrganismo().toEntity();
-
-    if (organismoEntity.getId() == null) {
-      var optional = organismoRepository.findBy(
-        Example.of(
-          organismoEntity,
-          ExampleMatcher.matching()
-            .withIgnorePaths("id", "peptideo", "origem", "nomepopular")
-            .withIgnoreCase()
-            .withStringMatcher(ExampleMatcher.StringMatcher.EXACT)
-        ),
-        q -> q.first()
-      );
-      if (optional.isPresent()) {
-        organismoEntity = optional.get();
-      }
-    }
-
-    organismoRepository.save(organismoEntity);
-
     var peptideoEntity = peptideoDTO.toEntity();
-    peptideoEntity.setOrganismo(organismoEntity);
+
+    // optionally persist organismo
+    if (peptideoDTO.getOrganismo() != null) {
+      var organismoEntity = peptideoDTO.getOrganismo().toEntity();
+
+      if (organismoEntity.getId() == null) {
+        var optional = organismoRepository.findBy(
+          Example.of(
+            organismoEntity,
+            ExampleMatcher.matching()
+              .withIgnorePaths("id", "peptideo", "origem", "nomepopular")
+              .withIgnoreCase()
+              .withStringMatcher(ExampleMatcher.StringMatcher.EXACT)
+          ),
+          q -> q.first()
+        );
+
+        if (optional.isPresent()) {
+          organismoEntity = optional.get();
+        }
+
+        peptideoEntity.setOrganismo(organismoEntity);
+      }
+
+      organismoRepository.save(organismoEntity);
+    }
+
     var saved = new PeptideoDTO(peptideoRepository.save(peptideoEntity));
-    return new ResponseEntity<>(saved, HttpStatus.CREATED);
+    return ResponseEntity.ok().body(saved);
   }
 
   @GetMapping
@@ -74,11 +75,15 @@ public class PeptideoController {
       .stream()
       .map(PeptideoDTO::new)
       .collect(Collectors.toList());
-    return new ResponseEntity<>(peptideos, HttpStatus.OK);
+    return ResponseEntity.ok().body(peptideos);
   }
 
   @GetMapping(path = { "/{id}" })
   public ResponseEntity<PeptideoDTO> findById(@PathVariable Long id) {
+    if (id == null) {
+      return ResponseEntity.badRequest().build();
+    }
+
     return peptideoRepository
       .findById(id)
       .map(record -> ResponseEntity.ok().body(new PeptideoDTO(record)))
@@ -87,23 +92,40 @@ public class PeptideoController {
 
   @PutMapping(value = "/{id}")
   public ResponseEntity<PeptideoDTO> update(
-    @PathVariable("id") Long id,
+    @PathVariable("id") Optional<Long> id,
     @RequestBody PeptideoDTO peptideoDTO
   ) {
+    if (id.isEmpty()) {
+      if (peptideoDTO.getId() == null) {
+        return ResponseEntity.badRequest().build();
+      }
+      id = Optional.of(peptideoDTO.getId());
+    }
+
+    peptideoDTO.setId(id.get());
+
     return peptideoRepository
-      .findById(id)
+      .findById(id.get())
       .map(peptideo -> {
-        peptideoDTO.setId(id);
         Peptideo toSave = peptideoDTO.toEntity();
+
         if (peptideoDTO.getOrganismo() instanceof OrganismoDTO o) {
           Organismo organismoToSave = o.toEntity();
-          organismoToSave.setId(peptideo.getOrganismo().getId());
-          organismoToSave.setPeptideo(peptideo.getOrganismo().getPeptideo());
+
+          if (peptideo.getOrganismo() != null) {
+            organismoToSave.setId(peptideo.getOrganismo().getId());
+            organismoToSave.setPeptideo(peptideo.getOrganismo().getPeptideo());
+          }
+
           toSave.setOrganismo(organismoToSave);
-        } else {
+
+          organismoRepository.save(toSave.getOrganismo());
+        } else if (peptideo.getOrganismo() != null) {
           toSave.setOrganismo(peptideo.getOrganismo());
+
+          organismoRepository.save(toSave.getOrganismo());
         }
-        organismoRepository.save(toSave.getOrganismo());
+
         return ResponseEntity.ok()
           .body(new PeptideoDTO(peptideoRepository.save(toSave)));
       })
@@ -116,20 +138,8 @@ public class PeptideoController {
       .findById(id)
       .map(record -> {
         peptideoRepository.deleteById(id);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok().body("peptideo apagado com sucesso.");
       })
       .orElse(ResponseEntity.notFound().build());
-  }
-
-  @GetMapping(path = { "tipoPeptideo/{tipoPeptideo}" })
-  public ResponseEntity<List<PeptideoDTO>> findByTipoPeptideo(
-    @PathVariable("tipoPeptideo") TipoPeptideo tipoPeptideo
-  ) {
-    List<PeptideoDTO> peptideos = peptideoRepository
-      .findByTipoPeptideo(tipoPeptideo)
-      .stream()
-      .map(PeptideoDTO::new)
-      .collect(Collectors.toList());
-    return ResponseEntity.ok(peptideos);
   }
 }
