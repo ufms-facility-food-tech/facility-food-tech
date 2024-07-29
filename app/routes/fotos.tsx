84e703892b0e71e97dab9d79623666d80247f9ea
caio <hi@caio.sh>
2024-07-29 05:21:00 -0300

    feat: view photos

--- app/routes/fotos.tsx
+++ app/routes/fotos.tsx
@@ -1,3 +1,50 @@
+import {
+  json,
+  useLoaderData,
+  type ClientLoaderFunctionArgs,
+} from "@remix-run/react";
+import { Container } from "~/components/container";
+import type { components } from "~/api-schema";
+
+export async function clientLoader({ request }: ClientLoaderFunctionArgs) {
+  const url = new URL(request.url);
+
+  const res = await fetch(`${url.origin}/api/images`);
+
+  if (!res.ok) {
+    console.error(res);
+    return [];
+  }
+
+  return json(await res.json());
+}
+
 export default function Fotos() {
-  return <div>TODO</div>;
+  const photos = useLoaderData<typeof clientLoader>() as Array<
+    components["schemas"]["ImageMetadata"]
+  >;
+
+  return (
+    <Container title="Fotos">
+      {/* TODO: fix max-height */}
+      <ul className="flex flex-wrap justify-center gap-2 md:max-h-[8500px] md:flex-col md:justify-start xl:max-h-[4800px] 2xl:max-h-[3500px]">
+        {photos.map(({ fileName, alt }) => (
+          <li
+            key={fileName}
+            className="bg-neutral-100 md:w-1/2 xl:w-1/3 2xl:w-1/4"
+          >
+            <label>
+              <img
+                className="h-full w-auto"
+                src={`/images/upload/${fileName}`}
+                alt={alt || ""}
+                loading="lazy"
+              />
+              {alt ? <span className="m-1">{alt}</span> : null}
+            </label>
+          </li>
+        ))}
+      </ul>
+    </Container>
+  );
 }
