61b4c0afd201f8ca39206f8995e10bc1fe40585b
caio <hi@caio.sh>
2024-07-30 10:40:01 -0300

    fix: user entity generating table with reserved keyword "user"

--- api/main/java/com/facility/model/User.java
+++ api/main/java/com/facility/model/User.java
@@ -6,10 +6,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.util.Set;
 import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
+@Table(name = "user_") // user is a reserved keyword in PostgreSQL
 public class User implements UserDetails {
 
   @Id
