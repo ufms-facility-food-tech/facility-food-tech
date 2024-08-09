cd683af356fcc5a681e01d04b27731002791d4a7
caio <hi@caio.sh>
2024-08-09 13:07:28 -0300

    feat: dropped java api

--- app/routes/admin.listar.tsx
+++ app/routes/admin.listar.tsx
@@ -1,31 +1,82 @@
-import { redirect, type ClientLoaderFunctionArgs } from "@remix-run/react";
+import { NavLink, useLoaderData } from "@remix-run/react";
+import { db } from "../db.server/connection";
 
-export async function clientLoader({ request }: ClientLoaderFunctionArgs) {
-  const jwt = window.localStorage.getItem("jwt");
-  if (!jwt) {
-    return redirect("/login");
-  }
-
-  const url = new URL(request.url);
-
-  const res = await fetch(`${url.origin}/api/peptideos`, {
-    headers: {
-      Authorization: `Bearer ${jwt}`,
+export async function loader() {
+  return await db.query.peptideoTable.findMany({
+    columns: {
+      id: true,
+      nomeIdentificador: true,
+      sequencia: true,
+    },
+    with: {
+      organismo: {
+        columns: {
+          nomesPopulares: true,
+          nomeCientifico: true,
+        },
+      },
     },
   });
-
-  if (!res.ok) {
-    return {
-      error: "failed to fetch data",
-    };
-  }
-
-  const data = await res.json();
-  return {
-    data,
-  };
 }
 
 export default function ListPanel() {
-  return null;
+  const data = useLoaderData<typeof loader>();
+
+  return (
+    <>
+      <div className="m-4 flex items-center justify-between">
+        <p>{data.length} registros encontrados</p>
+      </div>
+
+      <div className="relative w-full overflow-x-auto rounded-lg outline outline-1">
+        <table className="w-full text-left">
+          <thead className="bg-neutral-100 text-lg font-bold outline outline-1">
+            <tr>
+              <th scope="col" className="w-12 px-4 py-3">
+                Identificação
+              </th>
+              <th scope="col" className="w-60 px-4 py-3">
+                Nome científico
+              </th>
+              <th scope="col" className="w-48 px-4 py-3">
+                Nome popular
+              </th>
+              <th scope="col" className="w-96 px-4 py-3">
+                Sequência
+              </th>
+              <th scope="col" className="px-4 py-3">
+                Detalhes
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map(({ nomeIdentificador, sequencia, organismo, id }) => (
+              <tr
+                key={id}
+                className="border-b odd:bg-neutral-50 even:bg-neutral-200"
+              >
+                <td className="px-4 py-4">{nomeIdentificador}</td>
+                <td className="px-4 py-4 italic">
+                  {organismo?.nomeCientifico}
+                </td>
+                <td className="px-4 py-4">
+                  {organismo?.nomesPopulares?.join(", ")}
+                </td>
+                <td className="text-wrap px-4 py-4">
+                  {sequencia && sequencia?.length > 15
+                    ? sequencia?.slice(0, 15).concat("...")
+                    : sequencia}
+                </td>
+                <td className="px-4 py-4">
+                  <NavLink to={`/peptideo/${id}`} className="underline">
+                    Visualizar
+                  </NavLink>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 }
---
+++ app/routes/fotos.upload.tsx
@@ -0,0 +1,119 @@
+import {
+  unstable_composeUploadHandlers as composeUploadHandlers,
+  unstable_createFileUploadHandler as createFileUploadHandler,
+  unstable_createMemoryUploadHandler as createMemoryUploadHandler,
+  NodeOnDiskFile,
+  unstable_parseMultipartFormData as parseMultipartFormData,
+  type ActionFunctionArgs,
+  json,
+  MaxPartSizeExceededError,
+} from "@remix-run/node";
+import { Form, useActionData, useNavigate } from "@remix-run/react";
+import { useState } from "react";
+import { TbFileUpload } from "react-icons/tb";
+import { SubmitButton, TextInput } from "~/components/form";
+import { db } from "~/db.server/connection";
+import { imageMetadataTable } from "~/db.server/schema";
+
+export async function action({ request }: ActionFunctionArgs) {
+  try {
+    const formData = await parseMultipartFormData(
+      request,
+      composeUploadHandlers(
+        createFileUploadHandler({
+          maxPartSize: 25_000_000, // 25MB
+          file: ({ filename }) => filename,
+          filter: ({ contentType, name }) =>
+            contentType.startsWith("image/") && name === "image",
+          avoidFileConflicts: true,
+          directory: `${process.env.UPLOAD_DIRECTORY}/images/upload`,
+        }),
+        // parse alt text into memory
+        createMemoryUploadHandler({
+          filter: ({ name }) => {
+            return name === "alt";
+          },
+        }),
+        // TODO: maybe this API will change in the future, should implement some tests
+        // or is the type checking from TS enough?
+        ({ filename, contentType }) => {
+          throw new Error(
+            `Falha ao realizar upload de "${filename}": Tipo de conteúdo "${contentType}" não é suportado.`,
+          );
+        },
+      ),
+    );
+    const file = formData.get("image");
+    if (!(file instanceof NodeOnDiskFile)) {
+      return json({ message: "Falha ao realizar upload da imagem", ok: false });
+    }
+
+    const alt = formData.get("alt");
+
+    db.insert(imageMetadataTable).values({
+      fileName: file.name,
+      alt: alt?.toString().trim(),
+    });
+
+    return json({ message: "Imagem enviada com sucesso", ok: true });
+  } catch (error) {
+    if (error instanceof MaxPartSizeExceededError) {
+      return json({
+        message: "Arquivo excedeu o tamanho máximo de 25MB",
+        ok: false,
+      });
+    }
+    if (error instanceof Error) {
+      return json({ message: error.message, ok: false });
+    }
+  }
+}
+
+export default function Upload() {
+  const data = useActionData<typeof action>();
+  const [filename, setFilename] = useState("");
+  const navigate = useNavigate();
+
+  return (
+    <div className="mb-4 flex flex-col items-center justify-center gap-2">
+      <Form
+        method="post"
+        encType="multipart/form-data"
+        className="w-full md:w-4/5 lg:w-3/5"
+      >
+        <label className="mb-2 flex h-36 w-full flex-col items-center justify-center gap-2 rounded-xl border border-dashed border-neutral-200 bg-neutral-50">
+          <TbFileUpload size="2rem" />
+          Selecionar foto...
+          {filename ? <p>Selecionado: {filename}</p> : null}
+          <input
+            type="file"
+            name="image"
+            accept="image/*"
+            className="hidden"
+            onChange={(e) =>
+              setFilename(e.target.files?.length ? e.target.files[0].name : "")
+            }
+          />
+        </label>
+        <TextInput label="Descrição da imagem" type="text" name="alt" />
+        <div className="m-4 flex items-center justify-center gap-2">
+          <SubmitButton>Enviar</SubmitButton>
+          <button
+            type="button"
+            className="rounded-full bg-neutral-200 px-6 py-2 text-lg font-bold hover:bg-red-600"
+            onClick={() => navigate(-1)}
+          >
+            Cancelar
+          </button>
+        </div>
+        {data ? (
+          <p
+            className={`${data.ok ? "bg-cyan-100 text-cyan-700" : "bg-red-50 text-red-800"} rounded-xl px-4 py-2 text-base`}
+          >
+            {data.message}
+          </p>
+        ) : null}
+      </Form>
+    </div>
+  );
+}
