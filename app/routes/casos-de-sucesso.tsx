0094a37e54c81d0dde3eac5ecdfa2288d2eda0d1
caio <hi@caio.sh>
2024-09-26 16:31:34 -0300

    feat: casos de sucesso listing

--- app/routes/casos-de-sucesso.tsx
+++ app/routes/casos-de-sucesso.tsx
@@ -1,9 +1,63 @@
+import { json } from "@remix-run/node";
+import { NavLink, useLoaderData } from "@remix-run/react";
+import { db } from "~/.server/db/connection";
 import { Container } from "~/components/container";
 
+export async function loader() {
+  const casosDeSucesso = await db.query.casoSucessoTable.findMany({
+    columns: {
+      id: true,
+      value: true,
+      peptideoId: true,
+    },
+    with: {
+      peptideo: {
+        columns: {
+          identificador: true,
+          sintetico: true,
+          palavrasChave: true,
+        },
+        with: {
+          organismo: {
+            columns: {
+              nomeCientifico: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  return json(casosDeSucesso);
+}
+
 export default function CasosDeSucesso() {
+  const casosDeSucesso = useLoaderData<typeof loader>();
+
   return (
     <Container title="Casos de Sucesso">
-      <p className="mt-4 text-neutral-800">TODO</p>
+      <ul className="mt-4 flex flex-col gap-6 rounded-lg bg-neutral-50 px-4 py-2 text-lg">
+        {casosDeSucesso.map(({ id, peptideoId, value, peptideo }) => (
+          <li key={id} className="flex flex-col gap-1">
+            <NavLink
+              to={`/peptideo/${peptideoId}`}
+              className="text-2xl font-bold text-cyan-600 hover:underline"
+            >
+              {peptideo.identificador
+                ? peptideo.identificador
+                : "(sem identificador)"}
+              {!peptideo.sintetico ? (
+                peptideo.organismo?.nomeCientifico ? (
+                  <i> - {peptideo.organismo.nomeCientifico}</i>
+                ) : (
+                  " - (sem nome científico)"
+                )
+              ) : null}
+            </NavLink>
+            <p className="text-xl italic">{value}</p>
+          </li>
+        ))}
+      </ul>
     </Container>
   );
 }
